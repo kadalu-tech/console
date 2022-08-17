f92748ffc0bbeabcf1eb5b34b94c655eb29d6743
aravindavk <aravindavk@users.noreply.github.com>
2022-08-17 13:16:06 +0000

    deploy: 3a7949a05a9557484a999796006d541c0aea79d2

---
+++ static/js/app.js
@@ -0,0 +1,36 @@
+function setCookie(name, value) {
+    document.cookie = `${name}=${value}; SameSite=Strict`;
+}
+
+function listCookies() {
+    var cookies = document.cookie.split(';');
+    return cookies.map(function (c) {
+        var name_value = c.trim().split("=");
+        return {
+            name: name_value[0],
+            value: name_value[1]
+        }
+    });
+}
+
+function getCookieValue(name) {
+    var cookies = listCookies();
+    for (var idx=0; idx<cookies.length; idx++) {
+        if (cookies[idx].name === name) {
+            return cookies[idx].value;
+        }
+    }
+
+    return null;
+}
+
+function getMgrCookieName(mgr_url, suffix) {
+    return `${encodeURIComponent(mgr_url)}${suffix}`;
+}
+
+function handleRedirectToLogin(mgr_url) {
+    var mgrApiToken = getCookieValue(getMgrCookieName(mgr_url, "-token"));
+    if (!mgrApiToken) {
+        location.href = `/login?mgr=${mgr_url}`;
+    }
+}

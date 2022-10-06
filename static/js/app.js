ce7093246224ef439ffcfe0ed616c8852922b33b
aravindavk <aravindavk@users.noreply.github.com>
2022-10-06 15:05:37 +0000

    deploy: 975ae93d671c40f3f9b992dfb6a27a47a36a6dda

--- static/js/app.js
+++ static/js/app.js
@@ -49,6 +49,7 @@ function authErrorRedirectHandle(mgrUrl, error) {
     if (error instanceof StorageManagerAuthError) {
         resetCookiesAndRedirectToLogin(mgrUrl);
     }
+    console.log(error)
 }
 
 // Return a SVG with the 16 colored rectangles grid
@@ -244,6 +245,14 @@ function volumeNameAndStatus(volume) {
     return `${volume.name} <span class="has-text-danger is-size-6 ml-4">${volume.state}, ${volume.metrics.health}</span>`;
 }
 
+function nodeNameAndStatus(node) {
+    if (node.state != "Up") {
+        return `${node.name} <span class="has-text-danger is-size-6 ml-4">${node.state}</span>`;
+    }
+
+    return `${node.name} <span class="has-text-success is-size-6 ml-4">${node.state}</span>`;
+}
+
 SVG_STOP = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 icon is-small"><path fill-rule="evenodd" d="M4.5 7.5a3 3 0 013-3h9a3 3 0 013 3v9a3 3 0 01-3 3h-9a3 3 0 01-3-3v-9z" clip-rule="evenodd" /></svg>';
 SVG_PLAY = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 icon is-small"><path fill-rule="evenodd" d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z" clip-rule="evenodd" /></svg>';
 SVG_DELETE = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 icon is-small"><path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z" clip-rule="evenodd" /></svg>';
@@ -267,6 +276,14 @@ function volumeDeleteButton(volume, idx) {
         return `<span class=""><i class="has-text-grey">${SVG_DELETE}</i> Delete</span>`;
     } else {
         return `<span class="is-clickable"><i class="has-text-danger">${SVG_DELETE}</i> Delete</span>`;
+    }
+}
+
+function nodeRemoveButton(node, idx) {
+    if (node.state != "Up") {
+        return `<span class=""><i class="has-text-grey">${SVG_DELETE}</i> Remove</span>`;
+    } else {
+        return `<span class="is-clickable"><i class="has-text-danger">${SVG_DELETE}</i> Remove</span>`;
     }
 }
 
@@ -306,4 +323,12 @@ function storageManagerFromCookies(mgrUrl) {
     var userId = getCookieValue(getMgrCookieName(mgrUrl, "-userid"));
     var apiKeyId = getCookieValue(getMgrCookieName(mgrUrl, "-api_key_id"));
     return StorageManager.fromToken(mgrUrl, userId, apiKeyId, token);
+}
+
+function volumesUrl(mgrUrl, pool) {
+    return '/volumes?mgr=' + mgrUrl + '&pool=' + pool.name;
+}
+
+function nodesUrl(mgrUrl, pool) {
+    return '/nodes?mgr=' + mgrUrl + '&pool=' + pool.name;
 }
